42b75c5d2368e5b348b3b9e3450d7795e82a4bb6
archith1915 <126151104+archith1915@users.noreply.github.com>
2025-11-30 00:11:14 +0530

    Stop tracking node_modules

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter', // <--- ADD THIS
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Edumate Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // <--- ADD THIS LINE
+      require('cypress-mochawesome-reporter/plugin')(on); 
+      return config;
     },
+    baseUrl: 'http://127.0.0.1:5500', // Update this to match your live server port
   },
 });
